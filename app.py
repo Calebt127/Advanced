63e879ddfa39fe38674ed8fbc66f337b04869b33
Calebt127 <calebdt1727@gmail.com>
2024-07-30 16:09:59 +0000

    Small Updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 col1, col2 = st.columns(2)
-col1.markdown("# This is just a placeholder. Happy Coding BSMP 🤖")
+col1.markdown("# Hello Fitness")
 col1.write("This is column 1.")
-col1.write("Hello World")
+col1.write("Hello Fitness")
